65a0b66887b53e66c63559e35b4a0d310e0d6781
gjevardat <greg.jdf@gmail.com>
2022-10-22 01:18:46 +0200

    add step function and bug fixes

--- utils.py
+++ utils.py
@@ -2,6 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
 
+#Compute and plot a f 2-step function starting at 0 on few period
+def step_function(dx,period,upperval,lowerval,num_periods):
+    num_samples_1p = int(period/dx)
+    x=np.linspace(0, num_periods*period, num_samples_1p*num_periods)
+    f=np.zeros_like(x)
+    a = 0 
+    inc = int(np.floor(num_samples_1p/2))
+    b = a + inc
+    for i in range(num_periods):
+        f[a:b]=upperval
+        f[b+1:b+1+inc]=lowerval
+        a = a+2*inc
+        b = b+2*inc
+    return x,f
+
 def square_function():
     dx = 0.01
     x = np.arange(-1, 1, dx)
@@ -30,34 +45,44 @@ def triangle_function(dx,P,x0):
     f[3 * nquart:] = 0
     return x, dx, f
 
-#Compute the first k fourier coefficients of a P periodic function 
-def compute_fourrier_coef(f,x,dx,P,k):
-    A = np.zeros([k])
-    B = np.zeros([k])
-    freqs = np.zeros([k])
-    H = np.zeros([k,len(x)])
-    SUM = np.zeros([len(x)])
-    
-    A[0] = np.sum(f*np.ones_like(x)* dx)  / (P)
-    print(A[0])
+def fourier_coeff(f,x,P,num_harmonics):
+    #We also store coefficient A0, B0
+    size = num_harmonics+1 
+    dx = 0.01
+    num_samples_1p = int(P/dx)
+    A = np.zeros([size])
+    B = np.zeros([size])
+    freqs = np.zeros([size])
+
+    A[0] = 2/P*np.sum(f[:num_samples_1p]*np.ones_like(x[:num_samples_1p]))*dx
     B[0] = 0 
-    for i in range(1,k):
-        freqs[i] = (i*np.pi/P)
-        A[i] = np.sum(f * np.cos(freqs[i]*x ))*dx/P
-        B[i] = np.sum(f * np.sin(freqs[i]*x ))*dx/P
-        H[i] = A[i] * np.cos((i*np.pi/P)*x)  + B[i] * np.sin((i*np.pi/P)*x)
+    freqs[0] = 0
+    for i in range(1,size):
+        #print('Compute '+ str(i)+'-th harmonic coefficient')
+        freqs[i] = (2*i*np.pi/P)
+        A[i] = (2/P)*np.sum(f[:num_samples_1p] * np.cos(freqs[i]*x[:num_samples_1p] ))*dx
+        B[i] = (2/P)*np.sum(f[:num_samples_1p] * np.sin(freqs[i]*x[:num_samples_1p] ))*dx
+    return A, B, freqs
+
+def fourier_series(f,x,P,num_harmonics):
+    A, B, freqs= fourier_coeff(f, x, P, num_harmonics)
+    #H0 stores constant function
+    size = num_harmonics+1 
+    H = np.zeros([size,len(x)])
+    SUM = np.zeros([len(x)])
+    H[0] = A[0]/2
+    SUM = H[0]
+    for i in range(1,size):
+        H[i] = A[i] * np.cos(freqs[i]*x)  + B[i] * np.sin((freqs[i])*x)
         SUM += H[i]
 
-    H[0] = A[0]/2
-    SUM += H[0]
-    
-    return A, B, freqs, H, SUM
+    return A, B, freqs, SUM, H
 
 #Utility function to plot fourier results
 def plot_fourier(f,x,A,B,freqs,H,SUM,num_harmonics):
     
-    plt.rcParams['figure.figsize'] = [12,12]
-    plt.rcParams.update({'font.size': 12})
+    plt.rcParams['figure.figsize'] = [8,8]
+    plt.rcParams.update({'font.size': 8})
     
     fig, ax = plt.subplots(2, 2)
     cmap = get_cmap('tab20')
@@ -69,7 +94,8 @@ def plot_fourier(f,x,A,B,freqs,H,SUM,num_harmonics):
     ax[0][0].plot(x, f)
     ax[0][0].set_title('Function to approximate')
     
-    for k in range(1,num_harmonics):
+    size = num_harmonics+1
+    for k in range(1,size):
         ax[0][1].plot(x, H[k])
         ax[0][1].set_title('Number of Harmonics = ' + str(num_harmonics))
     
@@ -77,7 +103,7 @@ def plot_fourier(f,x,A,B,freqs,H,SUM,num_harmonics):
     ax[1][0].set_title('Fourier series')
 
     #Plot harmonics A coefficients ( frequencies)
-    ax[1][1].plot( freqs[1:],A[1:])
+    ax[1][1].plot( freqs,A,'.')
     ax[1][1].set_title('Fourier coefficients')
     plt.tight_layout()
     plt.show()
